de9f8366ce434e2f489467d4a558c0bbf51e132d
Brandon Aaron <hello.brandon@aaron.sh>
2013-11-20 11:54:27 -0500

    Allow preventDefault and stopPropagation to function for events that are ignore due to throttling or intent delay.

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -180,6 +180,8 @@
                 settings = handleObj.data.mousewheel,
                 oldHandler = handleObj.handler,
                 newHandler = function(event) {
+                    if (settings.preventDefault === true) { event.preventDefault(); }
+                    if (settings.stopPropagation === true) { event.stopPropagation(); }
                     if (hasIntent) {
                         oldHandler.apply(elem, arguments);
                     }
@@ -199,6 +201,8 @@
                 settings = handleObj.data.mousewheel,
                 oldHandler = handleObj.handler,
                 newHandler = function(event) {
+                    if (settings.preventDefault === true) { event.preventDefault(); }
+                    if (settings.stopPropagation === true) { event.stopPropagation(); }
                     var args = arguments;
                     var delayed = function() {
                         oldHandler.apply(elem, args);
